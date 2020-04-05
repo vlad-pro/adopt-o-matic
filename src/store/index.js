6a033c2ac1bfe8ac46c6eec14bb98a475e32a02c
vproskurin <vladimir.proskurin@gmail.com>
2020-04-04 20:59:59 -0700

    Basic browsing functionality for vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// import actions from '.actions/actions'
+// import mutations from './mutations'
+import state from './state'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state,
+  // mutations,
+  // actions,
   modules: {
   }
 })
---
+++ src/views/Pet.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{ pet().name }}
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  methods: {
+    pet () {
+      const animal = this[this.$route.params.species][this.$route.params.id]
+      return animal
+    }
+  }
+}
+</script>
